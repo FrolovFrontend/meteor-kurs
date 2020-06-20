74ccd60fc28589891c9242f4b2f65ec9ef64d586
Maxim Frolov <buterbrodhs@gmail.com>
2020-06-20 22:05:08 +0400

    update 20.06.2020

--- src/blocks/modules/production-charts/production-charts.js
+++ src/blocks/modules/production-charts/production-charts.js
@@ -1,13 +1,85 @@
-modules.define('production-chart', ['i-bem-dom'], function(provide, bemDom) {
+import Chart from 'chart.js';
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
+const ctx1 = document.getElementById('chart-1');
+const ctx2 = document.getElementById('chart-2');
+const ctx3 = document.getElementById('chart-3');
 
+let myChart1 = new Chart(ctx1, {
+  type: 'line',
+  data: {
+    datasets: [
+      {
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
+});
+let myChart2 = new Chart(ctx2, {
+  type: 'line',
+  data: {
+    datasets: [
+      {
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
+});
+
+let myChart3 = new Chart(ctx3, {
+  type: 'line',
+  data: {
+    datasets: [
+      {
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: true,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
 });
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,4 @@
 import '%modules%/header/header';
 import '%modules%/home-preview/home-preview';
+import '%modules%/production-charts/production-charts';
 import '%modules%/footer/footer';
